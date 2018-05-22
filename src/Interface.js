074bed7729cf196f4b796f868b6aa0a104a03f25
nico <nicocofer@hotmail.com>
2018-05-22 14:17:20 +0200

    ajout interface component

---
+++ src/Interface.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Interface extends Component {
+  render() {
+    return (
+      <div>
+      <header className="App-header">
+          <h1 className="App-title">App Map Cahors</h1>
+        </header>
+        coucou
+      </div>
+    );
+  }
+}
+
+export default Interface;
