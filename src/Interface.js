ada105553b4ba00b516188f5cdc69c0b4d9a22bd
Sophiequehen <sophie.quehen@gmail.com>
2018-05-22 16:22:13 +0200

    ajout du menu mouvant

--- src/Interface.js
+++ src/Interface.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Interface extends Component {
+  displayInt(){
+    var divInt = document.getElementById('Int');
+    var btnInt = document.getElementById('Int-btn');
+    
+    if (divInt.style.width === "400px") {
+      divInt.style.width = "0";
+      btnInt.classList.add('mg-left');
+    }else{
+      divInt.style.width = "400px";
+      btnInt.classList.remove('mg-left');
+
+    }
+  }
   render() {
     return (
       <div>
-      <header className="App-header">
-          <h1 className="App-title">App Map Cahors</h1>
-        </header>
-        coucou
+      <div id="Int" className="Int">
       </div>
-    );
+      <button id="Int-btn" className="Int-btn" onClick={this.displayInt}></button>
+      </div>
+      );
   }
 }
 
