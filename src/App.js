335a91e71b73d4d47e76cb5b3c7ecf4a819d77df
nico <nicocofer@hotmail.com>
2018-05-22 15:26:35 +0200

    ajout de la carte

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Interface from './Interface';
+import Carte from './Carte';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Interface />
+        <Carte />
       </div>
     );
   }
