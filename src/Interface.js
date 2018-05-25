1c2229a5cb993047ded1a5642bb15e2b8052e870
Sophiequehen <sophie.quehen@gmail.com>
2018-05-25 14:32:15 +0200

    amélioration design

--- src/Interface.js
+++ src/Interface.js
@@ -44,8 +44,6 @@ class Interface extends Component {
     }
   }
   
-
-
   getCoord(id,lieux){
 
     for (var i = 0; i < tab.length; i++) {    
@@ -88,24 +86,28 @@ class Interface extends Component {
 displayInt(){
   var divInt = document.getElementById('Int');
   var btnInt = document.getElementById('Int-btn');
+  var arrow = document.getElementById('arrow');
 
   if (divInt.style.left === "0px") {
-    divInt.style.left = "-400px";
+    divInt.style.left = "-350px";
     btnInt.classList.add('left'); 
+    arrow.innerHTML = 'keyboard_arrow_right';
   }else{
     divInt.style.left = "0px";
     btnInt.classList.remove('left');
+    arrow.innerHTML = 'keyboard_arrow_left';
   }
 }
 
 displaySouscat(i){
   var list =  document.getElementById('list'+i);
+  var addCat = document.getElementById('add-cat'+i);
   if ( list.classList.contains("display-none")) {
     list.classList.remove('display-none');
-    
+    addCat.innerHTML = "remove";
   }else{
     list.classList.add('display-none');
-
+    addCat.innerHTML = "add";
   }
 }
 
@@ -113,17 +115,25 @@ render() {
  return (
   <div>
   <div id="Int" className="Int" style={{left: 0 }}>
+  <div className="Int-header">
+  <h2>VILLE DE CAHORS</h2>
+  </div>
 
   {  places.map((place, i) =>  
     <div key={place + i }>
-    <h2 className="Int-cat" id={i} onClick={() => this.displaySouscat(i)}>{place.name}</h2>
+    <h2 className="Int-cat" id={i} onClick={() => this.displaySouscat(i)}>
+    <i id={"add-cat"+i} class="material-icons">add</i>
+    {(place.name).toUpperCase()}
+    </h2>
     <ul id={'list'+i} className="display-none"> {  place.children.map((child, j) => 
-      <li key={child+j}  id={child.id} onClick={() => this.getCoord(child.id,places)}><i id={"img#"+child.id} className="material-icons eye">
-      visibility_off</i>{child.name}</li>)} 
+      <li key={child+j}  id={child.id} onClick={() => this.getCoord(child.id,places)}>
+      <i id={"img#"+child.id} className="material-icons eye">visibility_off</i>
+      {child.name}
+      </li>)} 
     </ul>
     </div> )}
   </div>
-  <button id="Int-btn" className="Int-btn" onClick={this.displayInt} ></button>
+  <button id="Int-btn" className="Int-btn" onClick={this.displayInt} ><i id="arrow" class="material-icons">keyboard_arrow_left</i></button>
 
   <div id="map-position">
   <Map center={this.state.mapCenter} zoom={this.state.zoomLevel}>
